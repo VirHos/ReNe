197d28ebe0f654a4cc8cad96826148df115c37f3
Бадертдинов Ибрагим <badertdinov.i.i@sberbank.ru>
2021-10-22 23:08:04 +0300

    user_history

--- get_data.sh
+++ get_data.sh
@@ -2,3 +2,4 @@ cd data
 wget https://transfer.sh/vLIUDH/dataset_news_1.xlsx -q
 wget https://transfer.sh/MRRlOm/news.jsn -q
 wget https://transfer.sh/KH4ezU/areas.json -q
+wget https://transfer.sh/kORtrM/user_history.json -q
