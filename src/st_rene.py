d493b8e1d5a6c9456ce6fe05287c37d159885a82
Бадертдинов Ибрагим <badertdinov.i.i@sberbank.ru>
2021-10-23 16:03:03 +0300

    flask st

--- src/st_rene.py
+++ src/st_rene.py
@@ -1,6 +1,7 @@
 import streamlit as st
 from utils import yaml_load
 from builder import build_rene
+import requests
 import json
 import pandas as pd
 
@@ -10,14 +11,17 @@ def main(config):
     st.sidebar.header('Меню')
     n_news = st.sidebar.number_input(label='Выберите количество рекомендуемых новостей', min_value= 1, max_value= 20, value= 3)
 
-    rene = build_rene(config)
+    #rene = build_rene(config)
 
     user_id = st.number_input(label='Введите user id', min_value= 1, max_value= 267, value= 5)
 
 
+    
+
     get_pred = st.button('Анализировать')
     if get_pred:
-        preds_dict = rene.get_news(str(user_id), n_news)
+        js = requests.get('http://rene:5000/get_news', params={'user_id': str(user_id), 'n_news': n_news})
+        preds_dict = json.loads(js.content)
         st.subheader('Последняя прочитанная новость')
         st.write(preds_dict['history'][-1])
 
