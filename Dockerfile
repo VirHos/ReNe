68aceac5a54386db9ae6c6187623ac044fb000a2
Бадертдинов Ибрагим <badertdinov.i.i@sberbank.ru>
2021-10-23 22:28:19 +0300

    update

--- Dockerfile
+++ Dockerfile
@@ -1,2 +1,3 @@
 FROM tensorflow/tensorflow:1.15.5-py3
+COPY requirements.txt /rene/requirements.txt
 RUN pip3 install -r requirements.txt
--- get_data.sh
+++ get_data.sh
@@ -3,4 +3,5 @@ wget https://transfer.sh/vLIUDH/dataset_news_1.xlsx -q
 wget https://transfer.sh/MRRlOm/news.jsn -q
 wget https://transfer.sh/KH4ezU/areas.json -q
 wget https://transfer.sh/kORtrM/user_history.json -q
-wget https://transfer.sh/40vpzc/labse_enc_ru_en.pbtxt
+wget https://transfer.sh/40vpzc/labse_enc_ru_en.pbtxt -q
+wget https://transfer.sh/pCNc00/cache.pkl -q
