4684dcfe1ad9d38f196e0fc2f52aec0d4671f8e6
Бадертдинов Ибрагим <badertdinov.i.i@sberbank.ru>
2021-10-23 13:39:54 +0300

    update container

--- src/st_rene.py
+++ src/st_rene.py
@@ -5,24 +5,23 @@ import json
 import pandas as pd
 
 def main(config):
+    st.sidebar.header('PyPyPy')
     st.sidebar.write("""Демо рекомендательной системы новостей для пользователей mos.ru и приложения “Моя Москва”""")
     st.sidebar.header('Меню')
-    n_news = st.sidebar.number_input(label='Выберите количетво новостей', min_value= 1, max_value= 20, value= 3)
+    n_news = st.sidebar.number_input(label='Выберите количество рекомендуемых новостей', min_value= 1, max_value= 20, value= 3)
 
     rene = build_rene(config)
 
-    user_id = st.number_input(label='Введите user id', min_value= 1, max_value= 267, value= 3)
+    user_id = st.number_input(label='Введите user id', min_value= 1, max_value= 267, value= 5)
 
 
     get_pred = st.button('Анализировать')
     if get_pred:
         preds_dict = rene.get_news(str(user_id), n_news)
-        st.subheader('История пользователя')
-        st.dataframe(pd.DataFrame(preds_dict['history']))
-        st.subheader('Рекомендации для пользователя')    
-        st.dataframe(pd.DataFrame(preds_dict['recommendations']))
+        st.subheader('Последняя прочитанная новость')
+        st.write(preds_dict['history'][-1])
 
-        st.subheader('Output json')
+        st.subheader('output json')
         st.write(preds_dict)
 
 
