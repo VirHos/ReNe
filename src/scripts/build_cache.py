e284428d412cbfb231260227046094a7a4dafbb0
Бадертдинов Ибрагим <badertdinov.i.i@sberbank.ru>
2021-10-24 12:54:42 +0300

    up

--- src/scripts/build_cache.py
+++ src/scripts/build_cache.py
@@ -1,14 +1,17 @@
 from builder import build_rene
 from utils import json_load, pickle_dump, yaml_load
+from tf_encoder.cache_encoder import get_nlu_executor
 
 cfg = yaml_load("data/config.yml")
 
-rene = build_rene(cfg)
+rene = build_rene(cfg, True)
+
+nlu = get_nlu_executor(yaml_load("data/model_config.yml"))
 
 tr = json_load("data/train_user.json")
 
 meta_str_list = list(rene.user_pr.meta_info.values())
 
-embs = rene.retriever.encoder(meta_str_list, verbose=True)
+embs = nlu(meta_str_list, verbose=True)
 
 pickle_dump({"text": meta_str_list, "embs": embs}, "data/cache.pkl")
