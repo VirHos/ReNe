a7f3158f0f18792c1d4505f708163d5f571be3e2
Бадертдинов Ибрагим <badertdinov.i.i@sberbank.ru>
2021-11-06 17:54:33 +0300

    cache update

--- get_data.sh
+++ get_data.sh
@@ -3,7 +3,7 @@ wget https://transfer.sh/9BJSFH/dataset_news_1.xlsx -q
 wget https://transfer.sh/NWi1DL/news.jsn -q
 wget https://transfer.sh/fQ59Yv/user_history.json -q
 wget https://transfer.sh/RqqnfX/narezka_last5_history_users.csv -q
-wget https://transfer.sh/MoQLtH/cache.pkl -q
+wget https://transfer.sh/0k1yJ8/cache.pkl -q
 wget https://transfer.sh/kdsLKL/rene_mlp.h5 -q
 wget https://transfer.sh/UhFQFA/rene_labse.pbtxt -q
 wget https://transfer.sh/bccvzk/news_for_history5.csv -q
